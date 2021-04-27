ee0e9b3bdede0c67e211074f01d3a1cad5e8fb92
Matt Schulte <schultetwin1@gmail.com>
2021-04-26 20:23:08 -0700

    Fix clippy issue

--- build.rs
+++ build.rs
@@ -10,18 +10,15 @@ fn main() {
     let out_dir = std::path::PathBuf::from(out_dir);
     let input = std::path::PathBuf::from(path_to_c_source);
     let output = out_dir.join("hello");
-    let mut cmd = if compiler.is_like_msvc() {
-        let mut cmd = compiler.to_command();
+    let mut cmd = compiler.to_command();
+    if compiler.is_like_msvc() {
         cmd.args(&[
             input.to_str().unwrap(),
             "/LINK",
             &format!("/OUT:{}", output.to_str().unwrap()),
         ]);
-        cmd
     } else {
-        let mut cmd = compiler.to_command();
         cmd.args(&[input.to_str().unwrap(), "-o", output.to_str().unwrap()]);
-        cmd
     };
 
     println!("Running command: {:?}", cmd);
