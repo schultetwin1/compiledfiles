187f3d015589c87ab34c588c22d64b908212a8f9
Matt Schulte <schultetwin1@gmail.com>
2021-04-26 18:58:04 -0700

    Added check for if binary has debug symbols to test
    Temporary check to verify the binary has debug symbols

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,15 +1,31 @@
+use object::Object;
 use spectral::prelude::*;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+
+fn has_debug_symbols<P>(path: P) -> bool
+where
+    P: AsRef<Path>,
+{
+    let mut bytes = vec![];
+    let mut file = std::fs::File::open(path).unwrap();
+    file.read_to_end(&mut bytes).unwrap();
+    let obj = object::File::parse(&bytes).unwrap();
+    obj.has_debug_symbols()
+}
 
 #[test]
 fn basic_executable() {
-    let root_dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let test_dir = root_dir.join("tests").join("c");
     let hello_source = test_dir.join("hello.c").canonicalize().unwrap();
 
-    let bin_path = std::path::PathBuf::from(env!("COMPILEDFILES_BASIC_TEST_BIN_PATH"));
+    let bin_path = PathBuf::from(env!("COMPILEDFILES_BASIC_TEST_BIN_PATH"));
 
-    let elf_file = std::fs::File::open(&bin_path).unwrap();
-    let files = compiledfiles::parse(elf_file).unwrap();
+    if has_debug_symbols(&bin_path) {
+        let bin_file = std::fs::File::open(&bin_path).unwrap();
+        let files = compiledfiles::parse(bin_file).unwrap();
 
-    assert_that!(files.iter().find(|&f| f.path == hello_source)).is_some();
+        assert_that!(files.iter().find(|&f| f.path == hello_source)).is_some();
+    }
 }
